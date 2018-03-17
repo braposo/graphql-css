ce3af740b3f43da53e33989546df133e086cf667
Bernardo Raposo <braposo@gmail.com>
2018-03-17 23:38:33 +0000

    Changes tag prop to component to be more accurate and updates comments

--- src/index.js
+++ src/index.js
@@ -7,15 +7,12 @@ const smoosh = object => {
     return Object.assign(
         {},
         ...(function _flatten(objectBit, path = "") {
-            //spread the result into our return object
             return [].concat(
-                //concat everything into one level
                 ...Object.keys(objectBit).map(
-                    //iterate over object
                     key =>
-                        typeof objectBit[key] === "object" //check if there is a nested object
-                            ? _flatten(objectBit[key]) //call itself if there is
-                            : { [key]: objectBit[key] } //append object with it’s path as key
+                        typeof objectBit[key] === "object"
+                            ? _flatten(objectBit[key])
+                            : { [key]: objectBit[key] }
                 )
             );
         })(object)
@@ -42,13 +39,12 @@ const resolver = (fieldName, root, args = {}, context, { resultKey }) => {
 };
 
 // Main function to generate glamorous component with styles
-const gqlCSS = styles => (query, tag) => {
+const gqlCSS = styles => (query, component = "div") => {
     const generatedStyles = graphql(resolver, query, styles, null, null);
     const smooshedStyles = smoosh(generatedStyles);
-    const component = tag || "div";
 
-    // Returns just smooshed styles if tag is false
-    if (tag === false) {
+    // Returns just smooshed styles if component is false
+    if (component === false) {
         return smooshedStyles;
     }
 
@@ -57,25 +53,25 @@ const gqlCSS = styles => (query, tag) => {
 };
 
 // Also export component for more declarative API
-export const GqlCSS = ({ styles, query, tag = "div", ...rest }) => {
+export const GqlCSS = ({ styles, query, component = "div", ...rest }) => {
     return (
         <Subscriber quiet={true} channel="graphqlcss">
             {contextStyles => {
-                const Component = gqlCSS(styles || contextStyles || "")(query).withComponent(tag);
+                const Component = gqlCSS(styles || contextStyles || "")(query, component);
                 return <Component {...rest} />;
             }}
         </Subscriber>
     );
 };
 
-// Export provider to broadcast styles through different components
+// Export provider to broadcast styles to child GqlCSS components
 export const GqlCSSProvider = ({ styles, children }) => (
     <Broadcast channel="graphqlcss" value={styles}>
         <span>{children}</span>
     </Broadcast>
 );
 
-// Export HOC
+// Export HOC - uses render props underneath, because it's awesome
 export const withGqlCSS = (styles, query) => Component => props => {
     return (
         <WithGqlCSS styles={styles} query={query}>
