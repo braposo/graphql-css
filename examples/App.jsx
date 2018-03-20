54f403bf8933f182187e1329751f03673b7aeff0
Bernardo Raposo <braposo@gmail.com>
2018-03-20 12:35:51 +0000

    Updates example app to use new variant feature

--- examples/App.jsx
+++ examples/App.jsx
@@ -6,38 +6,42 @@ import glamorous from "glamorous";
 
 const h2Styles = gql`
     {
-        typography {
-            fontSize: scale {
-                l
+        base {
+            typography {
+                fontSize: scale {
+                    l
+                }
+                fontWeight: weight {
+                    bold
+                }
             }
-            fontWeight: weight {
-                bold
+            marginLeft: spacing {
+                xl
             }
-        }
-        marginLeft: spacing {
-            xl
-        }
-        color: colors {
-            green
+            color: colors {
+                green
+            }
         }
     }
 `;
 
 const h1Styles = gql`
     fragment H1 on styles {
-        typography {
-            fontSize: scale {
-                xl
+        base {
+            typography {
+                fontSize: scale {
+                    xl
+                }
+                fontWeight: weight {
+                    bold
+                }
             }
-            fontWeight: weight {
-                bold
+            marginLeft: spacing {
+                l
             }
-        }
-        marginLeft: spacing {
-            l
-        }
-        color: colors {
-            red
+            color: colors {
+                red
+            }
         }
     }
 `;
@@ -46,32 +50,67 @@ const customH1Styles = gql`
     ${h1Styles}
     {
         ...H1
-        marginLeft: spacing(unit: "em") {
-            s
-        }
-        color: colors {
-            blue
+        base {
+            marginLeft: spacing(unit: "em") {
+                s
+            }
+            color: colors {
+                blue
+            }
         }
     }
 `;
+
+const todoStyles = gql`
+    {
+        todo(variant: $variant) {
+            text
+        }
+    }
+`;
+
+class StateComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            variant: "normal",
+        };
+        this.toggleVariant = this.toggleVariant.bind(this);
+    }
+
+    toggleVariant() {
+        this.setState(state => ({
+            variant: state.variant === "normal" ? "done" : "normal",
+        }));
+    }
+
+    render() {
+        return (
+            <GqlCSS
+                styles={styles}
+                query={todoStyles}
+                variables={{ variant: this.state.variant }}
+                onClick={this.toggleVariant}
+            >
+                Using stateful component
+            </GqlCSS>
+        );
+    }
+}
 
 // Generates H2 glamorous component
 const H2 = gqlCSS(styles)(h2Styles, "h2");
 
 // Simulates existing H2 component
-const H2Comp = (props) => <h2 {...props} />
+const H2Comp = props => <h2 {...props} />;
 
 // Extracts styles object
 const inlineStyles = gqlCSS(styles)(h2Styles, false);
 
 // Simulates existing component that is enhanced by the HOC
-const ExistingComponent = ({ gqlStyles, ...rest }) => (
-    <glamorous.Div css={gqlStyles} {...rest} />
-);
+const ExistingComponent = ({ gqlStyles, ...rest }) => <glamorous.Div css={gqlStyles} {...rest} />;
 
 const HOCStyledComponent = withGqlCSS(styles, customH1Styles)(ExistingComponent);
-
-
 
 const App = () => (
     <div>
@@ -79,6 +118,7 @@ const App = () => (
         <GqlCSS styles={styles} query={h2Styles} component={H2Comp}>
             Using component
         </GqlCSS>
+        <StateComponent />
         <GqlCSSProvider styles={styles}>
             <div>
                 <div>
