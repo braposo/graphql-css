3a6955564a8732b19f5714482fc61285d710d29c
Bernardo Raposo <braposo@gmail.com>
2018-03-20 12:34:49 +0000

    Adds support for variables and variant arg to extract different versions of the styles

--- src/index.js
+++ src/index.js
@@ -32,10 +32,15 @@ const resolver = (fieldName, root = {}, args = {}, context, { resultKey }) => {
     let res = root[fieldName];
     const rootUnit = root && root.unit;
     const argsUnit = args && args.unit;
+    const argsVariant = args && args.variant;
 
     if (argsUnit || rootUnit) {
         const unit = argsUnit || rootUnit;
         res = root[fieldName] + unit;
+    }
+
+    if (argsVariant) {
+        res = root[fieldName][argsVariant];
     }
 
     // if has prop then use it as the key
@@ -49,8 +54,8 @@ const resolver = (fieldName, root = {}, args = {}, context, { resultKey }) => {
 };
 
 // Main function to generate glamorous component with styles
-const gqlCSS = styles => (query, component = "div") => {
-    const generatedStyles = graphql(resolver, query, styles, null, null);
+const gqlCSS = styles => (query, component = "div", variables) => {
+    const generatedStyles = graphql(resolver, query, styles, null, variables);
     const smooshedStyles = smoosh(generatedStyles);
 
     // Returns just smooshed styles if component is false
@@ -59,15 +64,15 @@ const gqlCSS = styles => (query, component = "div") => {
     }
 
     // Create glamorous component
-    return glamorous(component)(smooshedStyles);
+    return glamorous(component || "div")(smooshedStyles);
 };
 
 // Also export component for more declarative API
-export const GqlCSS = ({ styles, query, component = "div", ...rest }) => {
+export const GqlCSS = ({ styles, query, component = "div", variables, ...rest }) => {
     return (
         <Subscriber quiet={true} channel="graphqlcss">
             {contextStyles => {
-                const Component = gqlCSS(styles || contextStyles || "")(query, component);
+                const Component = gqlCSS(styles || contextStyles || "")(query, component, variables);
                 return <Component {...rest} />;
             }}
         </Subscriber>
@@ -82,17 +87,17 @@ export const GqlCSSProvider = ({ styles, children }) => (
 );
 
 // Export HOC - uses render props underneath, because it's awesome
-export const withGqlCSS = (styles, query) => Component => props => {
+export const withGqlCSS = (styles, query, variables) => Component => props => {
     return (
-        <WithGqlCSS styles={styles} query={query}>
+        <WithGqlCSS styles={styles} query={query} variables={variables}>
             {({ gqlStyles }) => <Component {...props} gqlStyles={gqlStyles} />}
         </WithGqlCSS>
     );
 };
 
 // Export render props component
-export const WithGqlCSS = ({ styles, query, children, ...rest }) => {
-    const processedStyles = gqlCSS(styles)(query, false);
+export const WithGqlCSS = ({ styles, query, variables, children, ...rest }) => {
+    const processedStyles = gqlCSS(styles)(query, false, variables);
     return children({ gqlStyles: processedStyles, ...rest });
 };
 
