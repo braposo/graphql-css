ddb36804d581d24a8fa699cc8ab8f58e37c141b2
Bernardo Raposo <braposo@gmail.com>
2018-03-21 11:06:29 +0000

    Adds support for GraphQL query variables and variant definition for the styleguide (#4)
    * Adds support for variables and variant argument in GraphQL queries to extract different versions of the styles (theming)
    * Updates examples with new variant option
    * Updates README with examples and adds headings for better structure

--- examples/App.jsx
+++ examples/App.jsx
@@ -1,59 +1,12 @@
 import React from "react";
 import styles from "./styleguide";
-import gqlCSS, { GqlCSS, GqlCSSProvider, withGqlCSS, WithGqlCSS } from "../lib";
+import gqlCSS, { GqlCSS, GqlCSSProvider, WithGqlCSS } from "../lib";
 import gql from "graphql-tag";
 import glamorous from "glamorous";
-
-const h2Styles = gql`
-    {
-        typography {
-            fontSize: scale {
-                l
-            }
-            fontWeight: weight {
-                bold
-            }
-        }
-        marginLeft: spacing {
-            xl
-        }
-        color: colors {
-            green
-        }
-    }
-`;
-
-const h1Styles = gql`
-    fragment H1 on styles {
-        typography {
-            fontSize: scale {
-                xl
-            }
-            fontWeight: weight {
-                bold
-            }
-        }
-        marginLeft: spacing {
-            l
-        }
-        color: colors {
-            red
-        }
-    }
-`;
-
-const customH1Styles = gql`
-    ${h1Styles}
-    {
-        ...H1
-        marginLeft: spacing(unit: "em") {
-            s
-        }
-        color: colors {
-            blue
-        }
-    }
-`;
+import HOCStyledComponent from "./HOC";
+import SubscriberComponent from "./SubscriberComponent";
+import StatefulComponent from "./StatefulComponent";
+import { h2Styles } from "./styleQueries";
 
 // Generates H2 glamorous component
 const H2 = gqlCSS(styles)(h2Styles, "h2");
@@ -64,15 +17,6 @@ const H2Comp = (props) => <h2 {...props} />
 // Extracts styles object
 const inlineStyles = gqlCSS(styles)(h2Styles, false);
 
-// Simulates existing component that is enhanced by the HOC
-const ExistingComponent = ({ gqlStyles, ...rest }) => (
-    <glamorous.Div css={gqlStyles} {...rest} />
-);
-
-const HOCStyledComponent = withGqlCSS(styles, customH1Styles)(ExistingComponent);
-
-
-
 const App = () => (
     <div>
         <H2>This is a styled text</H2>
@@ -80,22 +24,14 @@ const App = () => (
             Using component
         </GqlCSS>
         <GqlCSSProvider styles={styles}>
-            <div>
-                <div>
-                    <span>
-                        <GqlCSS query={h2Styles}>Using provider</GqlCSS>
-                    </span>
-                </div>
-            </div>
-            <GqlCSS query={h1Styles} css={{ marginTop: 30 }}>
-                Also using provider
-            </GqlCSS>
+            <SubscriberComponent/>
         </GqlCSSProvider>
         <h2 style={inlineStyles}>Inline styles</h2>
         <HOCStyledComponent>HOC Styled Component</HOCStyledComponent>
         <WithGqlCSS styles={styles} query={h2Styles}>
             {({ gqlStyles }) => <div style={gqlStyles}>Render props component</div>}
         </WithGqlCSS>
+        <StatefulComponent />
     </div>
 );
 
